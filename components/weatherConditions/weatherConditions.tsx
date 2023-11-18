41632fab83034d52f0c2a5387af92853b498b025
César Cabral <cesarcabral@cesarcabral.com>
2023-11-18 16:42:55 +0100

    feat: Add new icons & refactor weather icons

--- components/weatherConditions/weatherConditions.tsx
+++ components/weatherConditions/weatherConditions.tsx
@@ -3,8 +3,20 @@
 // SOURCE: https://codepen.io/front-end-developer/pen/vREwzJ
 
 import useMoonPhase from "@/hooks/useMoonPhase";
-import Moon from "./moonIcon/moonIcon";
-import styles from "./weatherConditions.module.scss";
+
+import IconWindy from "./watherIcon/iconWindy";
+import IconFog from "./watherIcon/iconFog";
+import IconSleet from "./watherIcon/iconSleet";
+import IconHail from "./watherIcon/iconHail";
+import IconRain from "./watherIcon/iconRain";
+import IconPartlyCloudyNight from "./watherIcon/iconPartlyCloudyNight";
+import IconClearNight from "./watherIcon/iconClearNight";
+import IconClearDay from "./watherIcon/iconClearDay";
+import IconSnow from "./watherIcon/iconSnow";
+import IconThunderstorm from "./watherIcon/iconThunderstorm";
+import IconPartlyCloudyDay from "./watherIcon/iconPartlyCloudyDay";
+import IconRainDay from "./watherIcon/iconRainSun";
+import IconRainNight from "./watherIcon/iconRainNight";
 
 export default function WeatherConditions() {
   const { moonPhasePercent } = useMoonPhase({
@@ -17,10 +29,10 @@ export default function WeatherConditions() {
   //    - [OK] partly-cloudy-day
   //    - [OK] partly-cloudy-night
   //    - [OK] cloudy
-  //    - fog
-  //    - wind
+  //    - [OK] fog
+  //    - [OK] wind
   //    - [OK] rain
-  //    - sleet (granizo)
+  //    - [OK] sleet (granizo)
   //    - [OK] snow
   //    - hail (chuva de granizo)
   //    - [OK] thunderstorm
@@ -28,69 +40,19 @@ export default function WeatherConditions() {
 
   return (
     <>
-      <div className={`${styles.icon} ${styles.sun_shower}`}>
-        <div className={styles.cloud}></div>
-        <div className={styles.sun}>
-          <div className={styles.rays}></div>
-        </div>
-        <div className={styles.rain}></div>
-      </div>
-
-      <div className={`${styles.icon} ${styles.thunder_storm}`}>
-        <div className={styles.cloud}></div>
-
-        <div className={styles.lightning}>
-          <div className={styles.bolt}></div>
-          <div className={styles.bolt}></div>
-        </div>
-      </div>
-
-      <div className={`${styles.icon} ${styles.cloudy}`}>
-        <div className={styles.cloud}></div>
-        <div className={styles.cloud}></div>
-      </div>
-
-      <div className={`${styles.icon} ${styles.flurries}`}>
-        <div className={styles.cloud}></div>
-        <div className={styles.snow}>
-          <div className={styles.flake}></div>
-          <div className={styles.flake}></div>
-        </div>
-      </div>
-
-      <div className={`${styles.icon} ${styles.sunny}`}>
-        <div className={styles.sun}>
-          <div className={styles.rays}></div>
-        </div>
-      </div>
-
-      <div className={`${styles.icon} ${styles.sunny}`}>
-        <Moon phase={moonPhasePercent} />
-      </div>
-
-      <div className={`${styles.icon} ${styles.sunny}`}>
-        <Moon phase={moonPhasePercent} />
-      </div>
-
-      <div className={`${styles.icon} ${styles.sunny}`}>
-        <div className={styles.cloud}></div>
-        <div className={styles.cloud}></div>
-
-        <Moon phase={moonPhasePercent} />
-        <div className={styles.rain}></div>
-      </div>
-
-      <div className={`${styles.icon} ${styles.flurries}`}>
-        <div className={styles.cloud}></div>
-        <div className={styles.cloud}></div>
-
-        <Moon phase={moonPhasePercent} />
-      </div>
-
-      <div className={`${styles.icon} ${styles.rainy}`}>
-        <div className={styles.cloud}></div>
-        <div className={styles.rain}></div>
-      </div>
+      <IconRainNight moonPhasePercent={moonPhasePercent} />
+      <IconRainDay />
+      <IconThunderstorm />
+      <IconPartlyCloudyDay />
+      <IconSnow />
+      <IconClearDay />
+      <IconClearNight moonPhasePercent={moonPhasePercent} />
+      <IconPartlyCloudyNight moonPhasePercent={moonPhasePercent} />
+      <IconRain />
+      <IconHail />
+      <IconSleet />
+      <IconFog />
+      <IconWindy />
     </>
   );
 }
